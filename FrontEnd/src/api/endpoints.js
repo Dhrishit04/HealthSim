26d7c8cf06519ef4a86aa37d4baeef298205291c
Dhrishit Seal <sealdhrishit@gmail.com>
2025-04-11 09:28:28 +0530

    mention references

--- FrontEnd/src/api/endpoints.js
+++ FrontEnd/src/api/endpoints.js
@@ -1,3 +1,6 @@
+export const AUTH_LOGIN = '/api/auth/login';
+export const AUTH_REGISTER = '/api/auth/register';
+
 export const endpoints = {
     login: '/auth/login',
     logout: '/auth/logout',
